1cc841a2ac97390a313155c7a6ab5290a898871e
Oleg G.Kapranov <lugatex@yahoo.com>
2017-09-15 20:16:29 +0300

    added models invitation, library

--- frontend/ember/yoember/app/controllers/index.js
+++ frontend/ember/yoember/app/controllers/index.js
@@ -46,8 +46,10 @@ export default Ember.Controller.extend({
       const newInvitation = this.store.createRecord('invitation', { email: email });
       newInvitation.save();
 
-      this.set('responseMessage', `Thank you! We have just saved your email address: ${this.get('emailAddress')}`);
-      this.set('emailAddress', '');
+      newInvitation.save().then((response) => {
+        this.set('responseMessage', `Thank you! We saved your email address with the following id: ${response.get('id')}`);
+        this.set('emailAddress', '');
+      });
     }
   }
 });
